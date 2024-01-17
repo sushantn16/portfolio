3c2798b20d0a378ca878d898711b04b76b9f8e42
sushantn16 <nandwani160698@gmail.com>
2024-01-17 18:10:04 +0100

    added text gradient and removed carousel

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,7 @@
 
-import { Card, CardContent, CardHeader } from "@/components/ui/card";
-import { Github, Mail, Linkedin, Download } from "lucide-react";
+import { Card, CardContent, CardFooter, CardHeader } from "@/components/ui/card";
+import { Github, Mail, Linkedin, Download, EyeIcon } from "lucide-react";
 import Link from "next/link";
-import {
-  Carousel,
-  CarouselContent,
-  CarouselItem,
-  CarouselNext,
-  CarouselPrevious,
-} from "@/components/ui/carousel"
 import {
   Tooltip,
   TooltipContent,
@@ -48,52 +41,62 @@ export default function Home() {
     {
       name: "Reservations",
       description: "This is a full stack application under way, which allows users to make reservations to a restraunt, also allow them to order food online. Users can also manage their orders and reservations here.",
-      href: "https://github.com/sushantn16/reservations"
+      href: "https://github.com/sushantn16/reservations",
+      github: "https://github.com/sushantn16/reservations"
     },
     {
       name: "Spectrum",
       description: "It is a dashboard application displaying mock sensor data from an aircraft. It also allows users to take actions whenever required. It is made using React and Typescript.",
-      href: "https://aerospace-data-dashboard.netlify.app/"
+      href: "https://aerospace-data-dashboard.netlify.app/",
+      github: "https://github.com/sushantn16/spectrum"
     },
     {
       name: "lifeOfSushant",
       description: "It is a simple personal blog website where I can share something abou my life. It is built using nextJs and deployed on Vercel. ",
-      href: "https://lifeofsushant.com/"
+      href: "https://lifeofsushant.com/",
+      github: "https://github.com/sushantn16/lifeofsushant"
     },
     {
       name: "Amacart",
       description: "This is an e-commerce website which allows users to shop and manage their account. It uses firebase for backend and is built using React. ",
-      href: "https://shopping-app-b9c1a.web.app/"
+      href: "https://shopping-app-b9c1a.web.app/",
+      github: "https://github.com/sushantn16/AmaKart"
     },
     {
       name: "Weather",
       description: "This is a simple weather checking application which uses mapbox api to help you find your city and open weather api to fetch the weather details.",
-      href: "https://weather-watcher11.netlify.app/"
+      href: "https://weather-watcher11.netlify.app/",
+      github: "https://github.com/sushantn16/weather"
     }
   ]
 
   return (
     <main className="min-h-screen p-10 md:p-24 font-mono bg-black text-white relative">
-      <TooltipProvider>
-        <Tooltip>
-          <TooltipTrigger>
-            <a
-              href='/Nandwani_Sushant.pdf'
-              target="_blank"
-              download='Sushant Nandwani.pdf'
-              className="duration-700 group fixed bottom-10 right-10">
-              <span className="relative z-10 flex items-center justify-center w-12 h-12 text-sm duration-1000 border rounded-full text-zinc-200 group-hover:text-white group-hover:bg-zinc-900 border-zinc-500 bg-zinc-900 group-hover:border-zinc-200 drop-shadow-orange">
-                <Download />
-              </span>
-            </a>
-          </TooltipTrigger>
-          <TooltipContent>
-            <p>Download Resume</p>
-          </TooltipContent>
-        </Tooltip>
-      </TooltipProvider>
+      <div className="fixed bottom-10 right-10">
+        <TooltipProvider>
+          <Tooltip>
+            <TooltipTrigger>
+              <a
+                href='/Nandwani_Sushant.pdf'
+                target="_blank"
+                download='Sushant Nandwani.pdf'
+                className="duration-700 group">
+                <span className="relative z-10 flex items-center justify-center w-12 h-12 text-sm duration-1000 border rounded-full text-zinc-200 group-hover:text-white group-hover:bg-zinc-900 border-zinc-500 bg-zinc-900 group-hover:border-zinc-200 drop-shadow-orange">
+                  <Download />
+                </span>
+              </a>
+            </TooltipTrigger>
+            <TooltipContent>
+              <p>Download CV</p>
+            </TooltipContent>
+          </Tooltip>
+        </TooltipProvider>
+      </div>
       <div className='flex flex-col items-center text-center'>
-        <p className='text-3xl md:text-5xl'>Sushant Nandwani</p>
+        <div className="gradient-bg">
+          <p className='text-3xl font-sans md:text-8xl gradient-text'>Sushant Nandwani</p>
+        </div>
+
         <p className='text-slate-300 text-center text-sm mt-2'>Software Engineer | Full-stack Developer</p>
         <p className='text-slate-300 text-center text-sm mt-2'>Koblenz, Germany</p>
         <div className="flex flex-row gap-8 justify-center mt-5">
@@ -116,26 +119,50 @@ export default function Home() {
         <p className="text-2xl">Projects</p>
         <p className="text-slate-300 mt-3">Some of the projects are from work and some are on my own time.</p>
         <div className="w-full h-px bg-zinc-800 mt-5" />
-          <Carousel className="mt-10 w-100">
-            <CarouselContent>
-              {projects.map((project, i) =>
-                <CarouselItem className="md:basis-1/3" key={i}>
-                  <Link href={project.href} className="duration-700 group" target="_blank">
-                    <Card className="bg-black h-80 border-zinc-500 group-hover:border-zinc-200 duration-1000 text-zinc-200 group-hover:text-white">
-                      <CardHeader className="text-xl">
-                        {project.name}
-                      </CardHeader>
-                      <CardContent className=" " >
-                        {project.description}
-                      </CardContent>
-                    </Card>
-                  </Link>
-                </CarouselItem>)}
-            </CarouselContent>
-            <CarouselPrevious className="bg-black border-0" />
-            <CarouselNext className="bg-black border-0" />
-          </Carousel>
-        <p className="text-slate-300 mt-5" >You can see more of my work on <Link className="underline" href={"https://github.com/sushantn16"} target="_blank">Github</Link>.</p>
+        <div className="grid grid-cols-3 gap-5 mt-10">
+          {projects.map((project, i) =>
+
+            <Card className="duration-700 group bg-black h-72 border-zinc-500 group-hover:border-zinc-200 duration-1000 text-zinc-200 group-hover:text-white flex flex-col justify-between" key={i}>
+              <div>
+                <CardHeader className="text-xl py-5">
+                  {project.name}
+                </CardHeader>
+                <CardContent className=" " >
+                  {project.description}
+                </CardContent>
+              </div>
+              <CardFooter className="p-3 justify-end gap-3">
+                <TooltipProvider>
+                  <Tooltip>
+                    <TooltipTrigger>
+                      <Link href={project.github}>
+                        <Github />
+                      </Link>
+                    </TooltipTrigger>
+                    <TooltipContent>
+                      <p>Open github repo</p>
+                    </TooltipContent>
+                  </Tooltip>
+                </TooltipProvider>
+                <TooltipProvider>
+                  <Tooltip>
+                    <TooltipTrigger>
+                      <Link href={project.href} target="_blank" >
+                        <EyeIcon />
+
+                      </Link>
+                    </TooltipTrigger>
+                    <TooltipContent>
+                      <p>Preview</p>
+                    </TooltipContent>
+                  </Tooltip>
+                </TooltipProvider>
+
+              </CardFooter>
+            </Card>)}
+          <p className="text-slate-300 mt-5" >You can see more of my work on <Link className="underline" href={"https://github.com/sushantn16"} target="_blank">Github</Link>.</p>
+        </div>
+
       </div>
 
       <div className="mt-10">
